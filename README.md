d45aec2e09ee4e37412b8ab119cfb410edaa87aa
paranid5 <dinaraparanid@gmail.com>
2025-02-23 15:08:25 +0300

    system overlay colors fix; local notifications for android release fix

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# HH.refresh
+# HH Refresh
+
+[![DiTest](https://github.com/dinaraparanid/hh_refresh/actions/workflows/di_test.yml/badge.svg)](https://github.com/dinaraparanid/hh_refresh/actions/workflows/di_test.yml)
+
+## **Developer**
+
+[Paranid5](https://github.com/dinaraparanid)
+
+## **About App**
+
+**HH Refresh** is a mobile application written with Flutter
+that schedules the reminder to promote your CV on hh.ru.
+Once time is up, you will receive the notification.
+
+## **Preview**
+
+![preview](assets/images/preview.png)
+
+## **Stack**
+
+<ul>
+    <li>Flutter 3.29.0</li>
+    <li>BLoC</li>
+    <li>Get It</li>
+    <li>Go Router</li>
+    <li>Rx Shared Preferences</li>
+    <li>Flutter Local Notifications</li>
+    <li>Freezed + Json Annotations</li>
+    <li>Intl + L10n</li>
+    <li>Flutter Timezone</li>
+    <li>Flutter Url Launcher</li>
+    <li>Flutter Test</li>
+</ul>
--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hh_refresh/core/presentation/theme/mod.dart';
 import 'package:hh_refresh/core/presentation/theme/theme_provider.dart';
@@ -15,11 +16,29 @@ final class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final brightness = MediaQuery.of(context).platformBrightness;
+
     final theme = AppTheme.create(
-      theme: switch (MediaQuery.of(context).platformBrightness) {
+      theme: switch (brightness) {
         Brightness.dark => AppThemeColors.dark,
         Brightness.light => AppThemeColors.light,
       },
+    );
+
+    final systemIconBrightness = switch (brightness) {
+      Brightness.dark => Brightness.light,
+      Brightness.light => Brightness.dark,
+    };
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: theme.colors.background,
+        systemNavigationBarDividerColor: theme.colors.background,
+        systemNavigationBarIconBrightness: systemIconBrightness,
+        systemNavigationBarContrastEnforced: true,
+        statusBarIconBrightness: systemIconBrightness,
+        systemStatusBarContrastEnforced: true,
+      )
     );
 
     return AppThemeProvider(
